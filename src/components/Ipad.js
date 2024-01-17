eedbc454313850d3172556a976ff62e0b35c17ef
jrnovera <jrnovera@gmail.com>
2024-01-18 03:31:18 +0800

    added new Capstone 3

---
+++ src/components/Ipad.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AccessoriesImages = () => {
+  return (
+    <div className="accessories-images-container">
+      <img
+        src="https://www.apple.com/v/ipad-pro/am/images/overview/keyboard-pencil/pencil_hover__f9n5c0u835qq_large.jpg"
+        alt="Accessories"
+        className="accessories-image"
+      />
+      {/* Add more images if needed */}
+    </div>
+  );
+};
+
+export default AccessoriesImages;
